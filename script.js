d1217c9afd0bcdbaa4959a4e8d92615e35cbf344
SihleMtshini <158051912+SihleMtshini@users.noreply.github.com>
2024-05-31 10:56:23 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+/* =========================== typing animation ============================ */
+var type = new Typed(".typing",{
+    strings:["","Web Designer","Full-Stack web Developer","Graphic Designer","DevOps Engineer"],
+    typeSpeed:100,
+    Backspeed:60,
+    loop:true
+})
+/* =========================== Aside ============================ */
